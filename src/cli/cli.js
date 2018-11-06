50f4c07a9105a560ef70550074bacbe8b189dd64
wasif <wasif@localhost.localdomain>
2018-11-07 00:16:12 +0500

    add properties parser

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
+const path = require('path');
+const read = require('./lib/read');
+const params = process.argv.slice(2);
+const source = params[0];
 
-console.log(process.argv.slice(2));
+read(path.resolve(source), { path: true, namespaces:true }, function(err, data) {
+    if (err) {
+        console.error(err);
+    }
 
+    console.log(data);
+})
 
 //var stdio = require('stdio');
-console.log(process.stdin);
+//console.log(process.stdin);
 /*var ops = process.stdin.getopt({
     'check': {key: 'c', args: 2, description: 'What this option means'},
     'map': {key: 'm', description: 'Another description'},
